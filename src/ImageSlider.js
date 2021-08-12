728f49b59d679d329b61870c018a293ad384e4a1
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-08-12 18:01:25 -0500

    commit tree e9a2732641a4af5f6245c155c8366dbb9cc5538b

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,27 @@
 // your ImageSlider code here!
+import React, { Component } from 'react'
+
+//It should only render out the text 'I am on slide <CURRENT_SLIDE>', 
+//where <CURRENT_SLIDE> is the value of this.state.currentSlideIndex.
+
+
+export default class ImageSlider extends Component {
+    constructor(){
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    // increment = () => {
+    //     const currentSlide = this.state.count + 1
+    //     this.setState({
+    //         count: currentSlide
+    //     })
+    // }
+
+    render() {
+        return <div>I am on slide {this.state.currentSlideIndex}</div>
+    }
+    
+}
